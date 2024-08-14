8b053747f28e8d52866119f239a2164289f9005d
Amanda Koster <amanda@aardvarkintel.com>
2024-08-14 15:50:51 -0400

    fixes key error in TollList map

--- src/components/TollList.tsx
+++ src/components/TollList.tsx
@@ -15,16 +15,15 @@ const TollList: React.FC<TollListProps> = ({
   return (
     <ul>
       {transactions.map((transaction) => (
-        <>
-          <li key={transaction.id}>
+        <div key={transaction.id}>
+          <li>
             <span>Vehicle ID: {transaction.vehicleId}</span>
             <span>Toll Booth ID: {transaction.tollBoothId}</span>
             <span>Amount Paid: ${transaction.amountPaid}</span>
           </li>
-
           <button onClick={() => onEdit(transaction)}>Edit</button>
-          <button onClick={() => onDelete(transaction.id)}>Edit</button>
-        </>
+          <button onClick={() => onDelete(transaction.id)}>Delete</button>
+        </div>
       ))}
     </ul>
   );
